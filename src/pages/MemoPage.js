869099976746dc0a13de4e8995ebc7cb9c36c5ea
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-12-22 17:05:38 +0900

    page created

---
+++ src/pages/MemoPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../util/reset.css";
+import styles from "./MemoPage.module.scss";
+
+const MemoPage = () => {
+  return <div>HELLO WORD</div>;
+};
+
+export default MemoPage;
