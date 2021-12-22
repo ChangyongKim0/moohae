3e065dc7aa07b0c3d6c0baebd7ead3c38a5e8d28
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-12-23 05:45:01 +0900

    bunch of modification

--- src/pages/MemoPage.js
+++ src/pages/MemoPage.js
@@ -1,29 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import classNames from "classnames/bind";
 import "../util/reset.css";
 import styles from "./MemoPage.module.scss";
 
 const cx = classNames.bind(styles);
 
-const title = ["무 해 한", "메 모 장"];
+const title = ["메모."];
+
+const reduceData = (state, action) => {
+  let new_state = [...state];
+  switch (action.type) {
+    case "add":
+      return [...new_state, { placeholder: action.data, id: Date.now() }];
+    case "remove":
+      return new_state.filter((e, idx) => idx != action.position);
+    case "remove end":
+      return new_state.slice(0, -1);
+    default:
+      return state;
+  }
+};
 
 const MemoPage = () => {
   const adjustTextareaHeight = () => {
     let elements = Array.from(document.getElementsByTagName("textarea"));
-    elements.forEach((element) => {
-      element.style.height = "10px";
+    let div_elements = Array.from(
+      document.getElementsByClassName("div-textarea")
+    );
+    console.log(div_elements);
+    // let length = elements.length;
+    elements.forEach((element, idx) => {
+      element.style.height = "5px";
       // element.scrollTop = 0;
-      element.style.height = element.scrollHeight + 10 + "px";
-      console.log(element.scrollHeight);
+
+      div_elements[idx].style.height = element.scrollHeight + "px";
+      element.style.height = element.scrollHeight + 50 + "px";
+      // console.log(element.scrollHeight);
+      if (
+        element.value == "" &&
+        idx < elements.length - 1 &&
+        elements.length > 2
+      ) {
+        console.log(idx);
+        handleData({ type: "remove", position: idx });
+      }
     });
+    console.log(elements.slice(-1)[0].value);
+    if (elements.slice(-1)[0].value != "") {
+      handleData({ type: "add", data: "메모를 추가하세요!" });
+    }
   };
   const getValue = (element) => {
     console.log(this);
   };
 
-  const data = ["dkdkdkdkd", "dddddd"];
+  const [data, handleData] = useReducer(reduceData, [
+    { placeholder: "메모를 추가하세요!", id: 0 },
+  ]);
 
-  useEffect(() => adjustTextareaHeight(), []);
+  useEffect(() => {
+    adjustTextareaHeight();
+    // document.getElementsByTagName("textarea")[0].value = <span>fff</span>;
+  }, []);
 
   return (
     <div className={cx("wrapper")}>
@@ -39,13 +77,18 @@ const MemoPage = () => {
         <div className={cx("frame-content")}>
           {data.map((e, idx) => {
             return (
-              <textarea
-                key={idx}
-                className={cx("content")}
-                placeholder={e}
-                spellCheck={false}
-                onKeyUp={adjustTextareaHeight}
-              ></textarea>
+              <div
+                key={e.id}
+                className={cx("frame-textarea") + " div-textarea"}
+              >
+                <textarea
+                  key={e.id}
+                  className={cx("content")}
+                  placeholder={e.placeholder}
+                  spellCheck={false}
+                  onKeyUp={adjustTextareaHeight}
+                ></textarea>
+              </div>
             );
           })}
         </div>
