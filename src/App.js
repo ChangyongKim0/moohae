9767f06ad8fd817c9471149b69d8823c6c37c28e
ChangyongKim0 <changyong.kim.0@gmail.com>
2022-01-01 12:19:51 +0900

    link added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MemoPage from "./pages/MemoPage";
+import HomePage from "./pages/HomePage";
+import MathEqPage from "./pages/MathEqPage";
+import MarkDownPage from "./pages/MarkDownPage";
+import ThreeJsPage from "./pages/ThreeJsPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<MemoPage />} />
+      <Route exact path="/" element={<HomePage />} />
+      <Route path="/memo" element={<MemoPage />} />
+      <Route path="/math-eq" element={<MathEqPage />} />
+      <Route path="/markdown" element={<MarkDownPage />} />
+      <Route path="/three-js" element={<ThreeJsPage />} />
     </Routes>
   );
 }
