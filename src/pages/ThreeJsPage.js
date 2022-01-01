8effec72cee40cfb5ac9cb269789f7cf2318a9d9
ChangyongKim0 <changyong.kim.0@gmail.com>
2022-01-01 15:02:24 +0900

    three js page created

--- src/pages/ThreeJsPage.js
+++ src/pages/ThreeJsPage.js
@@ -1,75 +1,49 @@
-import React, { useEffect } from "react";
+import React from "react";
 import classNames from "classnames/bind";
 import "../util/reset.css";
 import styles from "./ThreeJsPage.module.scss";
-
-import * as THREE from "three";
-import oc from "three-orbit-controls";
+import { Link } from "react-router-dom";
+import TitleBox from "../components/TitleBox";
 
 const cx = classNames.bind(styles);
 
-const OrbitControls = oc(THREE);
-
 const ThreeJsPage = () => {
-  const size = { width: window.innerWidth, height: window.innerHeight };
-
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    size.width / size.height,
-    1,
-    1000
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("title")}>THREE.JS.</div>
+      {/* <div className={cx("frame-logo")}>
+        <img
+          src="images/xprnc-revert.png"
+          alt="Experience"
+          className={cx("logo-xprnc")}
+        />
+        <div className={cx("logo-times")}>×</div>
+        <img
+          src="images/moohae.jpg"
+          alt="Experience"
+          className={cx("logo-moohae")}
+        />
+      </div> */}
+      <div className={cx("frame-button")}>
+        <Link to="/three-js/torus">
+          <TitleBox
+            title="Torus."
+            icon="images/threejs-02.jpg"
+            summary="움직이는 도넛"
+            color="#333333"
+          />
+        </Link>
+        <Link to="/">
+          <TitleBox
+            title="뒤로가기."
+            icon="images/moohae.jpg"
+            summary=""
+            color="#555555"
+          />
+        </Link>
+      </div>
+    </div>
   );
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-  camera.position.z = 5;
-  scene.add(camera);
-
-  renderer.setSize(size.width, size.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-  renderer.setClearColor(new THREE.Color("#21282a"), 1);
-
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff80 });
-  const cube = new THREE.Mesh(geometry, material);
-
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.minDistance = 1;
-  controls.maxDistance = 500;
-
-  scene.add(cube);
-
-  const animate = () => {
-    requestAnimationFrame(animate);
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    controls.update();
-    renderer.render(scene, camera);
-  };
-
-  //   renderer.render(scene, camera);
-
-  animate();
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      size.width = window.innerWidth;
-      size.height = window.innerHeight;
-      // Update camera
-      camera.aspect = size.width / size.height;
-      camera.updateProjectionMatrix();
-
-      // Update renderer
-      renderer.setSize(size.width, size.height);
-      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    });
-    document
-      .getElementsByClassName("three-js-container")[0]
-      .append(renderer.domElement);
-  }, []);
-
-  return <div className={cx("wrapper") + " three-js-container"}></div>;
 };
 
 export default ThreeJsPage;
